23216ad5add48f819038d15122b92b641ec19cc3
Kcire42 <erickcarrillo2807@gmail.com>
2025-10-09 21:56:02 -0600

    initial structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tiktok/config/theme/app_theme.dart';
+import 'package:tiktok/presentation/providers/discover_provider.dart';
+import 'package:tiktok/presentation/screens/discover/discover_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,14 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> DiscoverProvider())
+      ],
+    child: MaterialApp(
       title: 'TikTok Clone',
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('TikTok Clone')),
-        body: const Center(child: Text('Hello World')),
-      ),
+      home: const DiscoverScreen()
+    )
     );
   }
 }
